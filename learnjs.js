12cb30322a52410479ec9d870227ba1aceb13cf8
윤인희(대학원생-디지털마케팅전공) <113741161+Innieee@users.noreply.github.com>
2022-11-12 02:51:43 +0000

    11.12 수업

---
+++ learnjs.js
@@ -0,0 +1,15 @@
+7=='7' /* ==같니? */
+true
+7==='7' /* === 자료의 형태까지 같니? */
+false
+
+6!=5 /* != 다르니? */
+true
+
+6!=='6'
+true
+
+6>=6
+true
+6=>6 /* 항상 위로부터 아래로 내려와야 하기에 인식 못함 */
+VM1123:1 Uncaught SyntaxError: Malformed arrow function parameter list
